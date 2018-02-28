63b1872716d793aa4ccdbc7614763a736df24682
lpereyrasantiago <34422960+lpereyrasantiago@users.noreply.github.com>
2018-02-28 01:40:32 -0300

    Update rach_loves_puppies.py

--- rach_loves_puppies.py
+++ rach_loves_puppies.py
@@ -17,6 +17,14 @@ API = tweepy.API(auth)
 def create_data_file(path):
     if os.path.isfile(path) == False:
         os.mknod("data.txt")
+        
+def create_quote_data(path):
+    if os.path.isfile(path) == False:
+        os.mknod("quotedata.txt")
+
+def store_updated_quotes(path, twitted_quote):
+    with open(path, 'a') as outfile:
+        json.dump(twitted_quote, outfile, ensure_ascii=False)
 
 def store_updated_images(twitted_image):
 
@@ -47,5 +55,43 @@ def twit_random_image():
         except(tweepy.error.TweepError):
             print("Something happened B.")
             continue
+            
+def twit_random_quote():
 
-twit_random_image()
+    data_path = "path_to_a_data_file" # Path to the file where you'll store the quotes you've already twitted.
+    text_path = "path_to_text" # Path to a .txt file where you should store your quotes. 
+    # In order to work propperly, they should be store with no spaces, one quote per line, such as
+    # xzx - Mark Twain
+    # Something - Rachmaninov
+    # Whatever - Grace Hopper,
+    # etc. Can be done differently, but changes in the script would be needed.
+
+    create_quote_data(data_path)
+
+    while True:
+        try: 
+            with open(text_path, "r") as f:
+                line_lecture = f.readlines()
+            random_quote = random.choice(line_lecture)
+            random_quote_stringcheck = '"' + random_quote + '"'
+            with open(data_path, "r") as f:
+                data_lecture = f.read()
+                if not random_quote_stringcheck in data_lecture:
+                    API.update_status(random_quote)
+                    store_updated_quotes(data_path, random_quote)
+                    print("Updated quote.")
+                    break
+                else:
+                    continue
+        except(tweepy.error.TweepError):
+            print("Something happened D")
+            continue
+
+def run_twits():
+
+    while True:
+        twit_random_quote()
+        twit_random_image()
+        time.sleep(7200)
+
+run_twits()
